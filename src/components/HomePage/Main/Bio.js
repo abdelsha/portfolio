7bf478d96d0306b023c7b5627e3acc8dcba15390
abdelsha <49315774+abdelsha@users.noreply.github.com>
2021-10-21 12:09:24 -0400

    small edits

--- src/components/HomePage/Main/Bio.js
+++ src/components/HomePage/Main/Bio.js
@@ -20,17 +20,17 @@ function Bio(props){
         </div>
         <div className={classes.Description}>
           <p>GitHub: https://github.com/abdelsha</p>
-          <p>Portfolio: bit.ly/Amrshakour</p>
+          <p>Portfolio: https://hindsite2020.ca/</p>
           <p>
-            Hi, My name is Amr. It is nice to meet you! My current goal is to
-            increase my soft skills and technical skills in Electronic and
-            Hardware Design and use them to contribute to a cause far greater
-            than I am. If you are looking for an energetic, Charismatic leader
+            Hi, My name is Amr. It is a pleasure to meet you! My current goal is to
+            increase my soft skills and technical skills in Software Engineering
+             and use them to contribute to a cause far greater
+            than myself. If you are looking for an energetic, Charismatic leader
             and a persistent team member who's hungry for knowledge and thrives
-            in challenging environments, then we will be a great fit and let's
+            in a challenging environments, then we will be a great fit and let's
             work together to innovate for a sustainable and better future! Visit
             my portfolio for a better understanding of where I am heading and
-            the steps I am taking to achieve my goal. bit.ly/Amrshakour.
+            the steps I am taking to achieve my goal. www.hindsite2020.ca.
           </p>
         </div>
       </CommonCard>
